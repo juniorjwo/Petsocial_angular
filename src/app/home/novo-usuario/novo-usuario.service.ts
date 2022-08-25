b822e2bd6cca11132e44d370237916250b273081
juniorjwo <junioroliveira.fab@gmail.com>
2022-08-25 01:28:36 -0300

    Reactive forms e validação padrão

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NovoUsuario } from './novo-usuario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario:NovoUsuario){
+    return this.httpClient.post('http://localhost:3000/user/signup',novoUsuario)
+  }
+}
