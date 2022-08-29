1310ad090f0bf210214e2c825909a9a3c4e412c4
juniorjwo <juniorjwo@gmail.com>
2022-08-29 15:15:33 -0300

    Finalizado

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -5,7 +5,7 @@ import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { UsuarioExisteService } from './usuario-existe.service';
-// import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -36,10 +36,10 @@ export class NovoUsuarioComponent implements OnInit {
 
         ],
         password: [''],
+      },
+      {
+        validators: [usuarioSenhaIguaisValidator],
       }
-      // {
-      //   validators: [usuarioSenhaIguaisValidator],
-      // }
     );
   }
 
